0c7f7d4305d81fe8fe52da5060d897b9d8ed31e2
Your Name <you@example.com>
2025-02-20 18:18:19 +0200

    first commit

--- app/api/products5/route.js
+++ app/api/products5/route.js
@@ -1,10 +1,9 @@
-  
 import clientPromise from '../../lib/mongodb'; // Adjust path as needed
 import { NextResponse } from 'next/server';
 
 export const revalidate = 10;
 
-export async function GET(  ) { 
+export async function GET() { 
   try {
     const client = await clientPromise; // Connect to MongoDB
     const db = client.db('test'); // Replace with your database name
@@ -12,8 +11,10 @@ export async function GET(  ) {
 
     const data = await collection.find({  
       arrival: "yes" 
-    }).toArray(); // Fetch all documents
- 
+    })
+    .sort({ _id: -1 }) // Sort by newest first
+    .toArray(); // Fetch all documents
+
     return NextResponse.json(data); // Return data as JSON
   } catch (error) {
     console.error('Error fetching data from MongoDB:', error);
