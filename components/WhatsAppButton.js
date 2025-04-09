adc1d1e50aa16b26b856f3bb1580d97f944dad24
Your Name <you@example.com>
2025-04-09 20:58:36 +0200

    first commit

--- components/WhatsAppButton.js
+++ components/WhatsAppButton.js
@@ -115,7 +115,7 @@ Delivery fee: $5.00
         const intentURL = `intent://send?phone=${phoneNumber}&text=${encodedMessage}#Intent;scheme=smsto;package=com.whatsapp.w4b;end`;
         return { url: intentURL, isAndroidIntent: true };
     } else {
-        const waUrl = `https://wa.me/${phoneNumber}`;
-        return { url: waUrl, isAndroidIntent: false };
+        const waUrl = `https://wa.me/9613066976`;
+        return { url: waUrl };
     }
 };
