680d396c1b87ddfb1926eecaba4af9e8eef9fdc0
Your Name <you@example.com>
2025-07-25 16:42:12 +0300

    first commit

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -32,43 +32,43 @@ const CarCard = ({ temp }: CarCardProps) => {
   );
 
   return (
-            <a
-          href={`/product?id=${_id}&&imgg=${imageUrl}`}
-          className="text-gray-900 font-semibold text-lg hover:text-blue-600"
-        >
-    <div className="w-[100px] sm:w-[300px] flex flex-col items-center p-3  relative">
-      {/* Badge */}
-      {category === "Hot Sale" && (
-        <img
-          src="https://res.cloudinary.com/drupytlml/image/upload/v1740521061/m5fzgzf1a4p6xn3bj1mn.png"
-          alt="Hot Sale Badge"
-          className="absolute w-16 h-16 top-2 left-2 z-10"
-        />
-      )}
-
-      {/* Image (square & zoomed to fill) */}
-      <div className="w-[100px] h-[100px] sm:w-[300px] sm:h-[300px] overflow-hidden">
-
-        <img
-          src={imageUrl}
-          alt={title}
-          className="w-full h-full object-cover" // <-- zoom/crop instead of white background
-        />
-      </div>
-
-      {/* Title & Price */}
-      <div className="mt-3 text-center">
-        <a
-          href={`/product?id=${_id}&&imgg=${imageUrl}`}
-          className="text-gray-900 font-semibold text-lg hover:text-blue-600"
-        >
-          {title}
-        </a>
-        {isCodeValid && (
-          <p className="text-blue-600 font-bold text-lg mt-1">${price}</p>
+    <a
+      href={`/product?id=${_id}&&imgg=${imageUrl}`}
+      className="text-gray-900 font-semibold text-lg hover:text-blue-600 "
+    >
+      <div className="w-[100px] sm:w-[230px] flex flex-col items-center p-3  relative">
+        {/* Badge */}
+        {category === "Hot Sale" && (
+          <img
+            src="https://res.cloudinary.com/drupytlml/image/upload/v1740521061/m5fzgzf1a4p6xn3bj1mn.png"
+            alt="Hot Sale Badge"
+            className="absolute w-16 h-16 top-2 left-2 z-10 "
+          />
         )}
+
+        {/* Image (square & zoomed to fill) */}
+        <div className="w-[100px] h-[100px] sm:w-[230px] sm:h-[230px] overflow-hidden">
+
+          <img
+            src={imageUrl}
+            alt={title}
+            className="w-full h-full object-cover ml-5 mr-5" // <-- zoom/crop instead of white background
+          />
+        </div>
+
+        {/* Title & Price */}
+        <div className="mt-3 text-center">
+          <a
+            href={`/product?id=${_id}&&imgg=${imageUrl}`}
+            className="text-gray-900 font-semibold text-lg hover:text-blue-600"
+          >
+            {title}
+          </a>
+          {isCodeValid && (
+            <p className="text-blue-600 font-bold text-lg mt-1">${price}</p>
+          )}
+        </div>
       </div>
-    </div>
     </a>
   );
 };
